92d0ec2158b2bc9d3ccf229a1c277e638b36cea3
Henrimartins250 <henrinkcstd@gmail.com>
2026-08-30 19:15:59 -0300

    feat: added wayland xdg portal to env.lua

--- lua/env.lua
+++ lua/env.lua
@@ -1,2 +1,7 @@
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+-- Explicitly tell desktop portals that you are running Hyprland
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
